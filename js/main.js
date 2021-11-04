2ae3df47a91719708399783ee3e8b66187e7d2c2
Joshua Chen <jcgter777@gmail.com>
2021-11-04 10:28:31 -0400

    Various changes and fixes, mainly key press recording added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 function genCode() {
     location.replace("play.html");
     var randNum = Math.round(Math.random()*90001+10000);
-    //document.getElementById("code").innerHTML = randNum;
-    //document.getElementById("code").classList.add("visible");
+    //document.getElementById("session").innerHTML = randNum;
 }
--- js/play.js
+++ js/play.js
@@ -1,8 +1,21 @@
-function start() {
-    document.addEventListener("click", sendClick);
-    function sendClick(event) {
-        var x = event.clientX;
-        var y = event.clientY;
-        document.getElementById("test").innerHTML = x+","+y;
-      }
+function recClicks() {
+    document.getElementById("game").addEventListener("click", (event) => {
+        let rect = event.target.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+        document.getElementById("test").innerHTML = x+", "+y;
+    }, false);
+}
+function recPresses() {
+    document.addEventListener('keydown', (event) => {
+        var name = event.key;
+        var code = event.code;
+        document.getElementById("test").innerHTML =`Key pressed ${name} Key code value: ${code}`;
+    }, false);
+
+    document.addEventListener('keyup', (event) => {
+        var name = event.key;
+        var code = event.code;
+        document.getElementById("test").innerHTML =`Key lifted ${name} Key code value: ${code}`;
+    }, false);
 }
