08872c84ebe12633aac79228fdd459d8177674ad
Joshua Chen <jcgter777@gmail.com>
2021-11-04 21:13:15 -0400

    Added code to get id from join, and some various changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-function genCode() {
+/*function genCode() {
     location.replace("play.html");
     var randNum = Math.round(Math.random()*90001+10000);
     //document.getElementById("session").innerHTML = randNum;
+}*/
+
+function search(ele) {
+    if(event.key === 'Enter') {
+        alert(ele.value);
+    }
 }
--- js/play.js
+++ js/play.js
@@ -3,19 +3,21 @@ function recClicks() {
         let rect = event.target.getBoundingClientRect();
         let x = event.clientX - rect.left;
         let y = event.clientY - rect.top;
-        document.getElementById("test").innerHTML = x+", "+y;
+        document.getElementById("testclick").innerHTML = x+", "+y;
     }, false);
 }
 function recPresses() {
     document.addEventListener('keydown', (event) => {
         var name = event.key;
         var code = event.code;
-        document.getElementById("test").innerHTML =`Key pressed ${name} Key code value: ${code}`;
+        document.getElementById("testpressdown").innerHTML =`Key pressed ${name} Key code value: ${code}`;
     }, false);
 
     document.addEventListener('keyup', (event) => {
         var name = event.key;
         var code = event.code;
-        document.getElementById("test").innerHTML =`Key lifted ${name} Key code value: ${code}`;
+        document.getElementById("testpressup").innerHTML =`Key lifted ${name} Key code value: ${code}`;
     }, false);
+
+    recClicks();
 }
