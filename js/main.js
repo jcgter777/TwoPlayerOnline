15a8d4a1403ad566436aac258d3f778d961659f7
Joshua Chen <jcgter777@gmail.com>
2021-11-05 22:00:20 -0400

    Clean-up

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-/*function genCode() {
-    location.replace("play.html");
-    var randNum = Math.round(Math.random()*90001+10000);
-    //document.getElementById("session").innerHTML = randNum;
-}*/
-
 function search(ele) {
     if(event.key === 'Enter') {
         alert(ele.value);
