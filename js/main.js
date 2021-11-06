db91736a4f4d695b0f621831188e8b7ed7a2ed90
Seamoose44499958 <seamoose44499958@gmail.com>
2021-11-05 21:56:39 -0400

    Webpage with some webserver integration

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
-/*function genCode() {
-    location.replace("play.html");
-    var randNum = Math.round(Math.random()*90001+10000);
-    //document.getElementById("session").innerHTML = randNum;
-}*/
+window.onload = function(){
+    const URL="http://localhost:8080/"
+    document.getElementById("create").addEventListener('click',function(){
+        fetch(URL + "sessions/create",{method:'POST'}).then(r => {
+            if(r.ok){
+                console.log("Success creating session");
+            }
+            else{
+                console.log("Error creating session");
+            }
 
-function search(ele) {
-    if(event.key === 'Enter') {
-        alert(ele.value);
-    }
-}
+            if(r.redirected){
+                window.location.href = r.url;
+            }
+        });
+    });
+
+    document.getElementById("inputID").addEventListener('submit',(ev)=>{
+
+        ev.preventDefault();
+       fetch(ev.target.action,{method:'POST',body: new URLSearchParams(new FormData(ev.target))}).then(r =>{
+           if(r.ok){
+               console.log('Form with id submitted successfully');
+           }
+           else{
+               console.log('Error submitting id');
+           }
+
+           if(r.redirected){
+               window.location.href = r.url;
+           }
+       })
+    });
+};
+
--- js/play.js
+++ js/play.js
@@ -1,23 +1,5 @@
-function recClicks() {
-    document.getElementById("game").addEventListener("click", (event) => {
-        let rect = event.target.getBoundingClientRect();
-        let x = event.clientX - rect.left;
-        let y = event.clientY - rect.top;
-        document.getElementById("testclick").innerHTML = x+", "+y;
-    }, false);
-}
-function recPresses() {
-    document.addEventListener('keydown', (event) => {
-        var name = event.key;
-        var code = event.code;
-        document.getElementById("testpressdown").innerHTML =`Key pressed ${name} Key code value: ${code}`;
-    }, false);
+const URL="http://localhost:8080/"
+window.onload = function(){
 
-    document.addEventListener('keyup', (event) => {
-        var name = event.key;
-        var code = event.code;
-        document.getElementById("testpressup").innerHTML =`Key lifted ${name} Key code value: ${code}`;
-    }, false);
 
-    recClicks();
-}
+};
